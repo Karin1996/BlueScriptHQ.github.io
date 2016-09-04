7e5dc8577a9f5126023a8951b4e2864fc579c6f3
GerritLuimstra <gerrit.luimstra@ziggo.nl>
2016-09-04 13:31:28 +0200

    Set up the navigation page

---
+++ andalusier/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Andalusier</title>
+</head>
+<body>
+  Welkom bij Andalusier! :)
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <title>BlueScript HQ Navigation Page</title>
+</head>
+<body>
+    <h2>Welcome</h2>
+    <p>
+      This is some static content! :)
+    </p>
+    <a href="andalusier/index.php">Ga naar andalusier!</a>
+</body>
+</html>
